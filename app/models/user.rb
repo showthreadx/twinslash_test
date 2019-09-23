e81f660c05167dce3cc97a41c7ccc32b80dd777c
showthreadx <showthreadx@gmail.com>
2019-09-23 08:42:35 +0300

    Fix password validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :email, :password, presence: true
+  validates :email, presence: true
+  validates :password, length: { minimum: 6 }, on: :create
   has_many :ads
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
