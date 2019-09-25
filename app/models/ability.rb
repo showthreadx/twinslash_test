c7fe6d2bdd6e324b4180f823d598e1a328768ebc
showthreadx <showthreadx@gmail.com>
2019-09-25 13:45:47 +0300

    Fix ability.rb && admin_ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,21 +5,10 @@ class Ability
     can :read, :all
 
     if user.present? && !user.admin?
-      can :create, Ad
-      can :user_ads, Ad
-      can :user_archive, Ad
-      can :delete_image, Ad
-      can :update, Ad, user_id: user.id, status: 0
-      can :destroy, Ad, user_id: user.id, status: 0
-      can :destroy, Ad, user_id: user.id, status: 1
-      can :destroy, Ad, user_id: user.id, status: 3
-      can :destroy, Ad, user_id: user.id, status: 4
-      can :update, Ad, user_id: user.id, status: 2
-      can :destroy, Ad, user_id: user.id, status: 2
-      can :destroy, Ad, user_id: user.id, status: 5
-      can :status_new, Ad, user_id: user.id, status: 0
-      can :status_new, Ad, user_id: user.id, status: 2
-      can :status_new, Ad, user_id: user.id, status: 5
+      can [:create, :user_ads, :user_archive, :delete_image], Ad
+      can :destroy, Ad, user_id: user.id
+      can :update, Ad, user_id: user.id, status: [0, 2]
+      can :status_new, Ad, user_id: user.id, status: [0, 2, 5]
       can :status_draft, Ad, user_id: user.id, status: 5
     end
   end
--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -3,16 +3,10 @@ class AdminAbility
   def initialize(user)
     if user && user.admin?
       can :access, :rails_admin
-      can :read, :dashboard
       can :read, :all
-      can :destroy, Ad
-      can :update, Ad
-      can :create, User
-      can :destroy, User
-      can :update, User
-      can :create, AdType
-      can :destroy, AdType
-      can :approve, Ad
+      can [:destroy, :update, :approve], Ad
+      can [:create, :destroy, :update], User
+      can [:create, :destroy], AdType
     end
   end
 end
