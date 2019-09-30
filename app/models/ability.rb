1036a51cb930d0c7912000e25c084cfd1635f332
showthreadx <showthreadx@gmail.com>
2019-09-30 13:31:24 +0300

    Fix status_new ability and buttons of _results partial

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       can [:create, :user_ads, :user_archive, :delete_image], Ad
       can :destroy, Ad, user_id: user.id
       can :update, Ad, user_id: user.id, status: [0, 2]
-      can :status_new, Ad, user_id: user.id, status: [0, 2, 5]
+      can :status_new, Ad, user_id: user.id, status: [0, 2]
       can :status_draft, Ad, user_id: user.id, status: 5
     end
   end
