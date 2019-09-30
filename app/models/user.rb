3a6f8b05823802695bfb6630bbc89097aeb1efed
showthreadx <showthreadx@gmail.com>
2019-09-30 08:15:42 +0300

    Small fixes for devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true
   validates :password, length: { minimum: 6 }, on: :create
-  has_many :ads
+  has_many :ads, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
